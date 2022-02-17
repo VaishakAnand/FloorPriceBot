88fe8ce6846d00c94111f1a6fc90948ac7cc66fb
VaishakDell\Vaishak <vaishak140498@gmail.com>
2022-02-17 20:57:53 +0800

    add heroku

--- controllers/checkPrice.js
+++ controllers/checkPrice.js
@@ -5,120 +5,58 @@ const getNormalTime = require('../util/datetime');
 let plsStop = false;
 let messageId = null;
 let priceFloor = 1;
-let chatId = null;
+let chatId = process.env.CHAT_ID;
 
-const check = async (ctx) => {
+const check = async () => {
     try {
-        if (plsStop) {
-            return;
-        }
-
         let price = await priceCheckFn();
-        let message = `Retrieval Time: ${getNormalTime()}\n\n`;
+        console.log("Price:", price);
 
-        if (percentage > 0) {
-            message += `Floor Price: ${price}`;
-            if (messageId == null && !plsStop) {
-                await bot.telegram
-                    .sendMessage(ctx.chat.id, message, {
+        let message = `\nRetrieval Time: ${getNormalTime()}\n\n`;
+
+        message += `Floor Price: ${price}`;
+        if (messageId == null && !plsStop) {
+            await bot.telegram
+                .sendMessage(chatId, message, {
+                    parse_mode: 'HTML',
+                })
+                .then((messageDetails) => {
+                    messageId = messageDetails.message_id;
+                })
+                .catch((err) => console.error(err));
+        } else {
+            await bot.telegram
+                .editMessageText(
+                    chatId,
+                    messageId,
+                    undefined,
+                    message,
+                    {
                         parse_mode: 'HTML',
-                    })
-                    .then((messageDetails) => {
-                        messageId = messageDetails.message_id;
-                    })
-                    .catch((err) => console.error(err));
-            } else {
-                await bot.telegram
-                    .editMessageText(
-                        ctx.chat.id,
-                        messageId,
-                        undefined,
-                        message,
-                        {
-                            parse_mode: 'HTML',
-                        }
-                    )
-                    .catch((err) => console.error(err));
-            }
-
-            if (percentage <= percentageFallFloor) {
-                let alertMessage = `<b>PERCENTAGE ALERT</b> @Vforvitagen\nPercentage: ${percentage}`;
-
-                await bot.telegram
-                    .sendMessage(ctx.chat.id, alertMessage, {
-                        parse_mode: 'HTML',
-                    })
-                    .catch((err) => console.error(err));
-            }
-
-            if (price <= priceFloor) {
-                let alertMessage = `<b>PRICE ALERT</b> @Vforvitagen\nFloor Price: ${price}`;
-
-                await bot.telegram
-                    .sendMessage(ctx.chat.id, alertMessage, {
-                        parse_mode: 'HTML',
-                    })
-                    .catch((err) => console.error(err));
-            }
+                    }
+                )
+                .catch((err) => console.error(err));
         }
 
-        setImmediate(() => check(ctx));
+        if (price <= priceFloor) {
+            let alertMessage = `<b>PRICE ALERT</b> @Vforvitagen\nFloor Price: ${price}`;
+
+            await bot.telegram
+                .sendMessage(chatId, alertMessage, {
+                    parse_mode: 'HTML',
+                })
+                .catch((err) => console.error(err));
+        }
+
+        setImmediate(() => check());
     } catch (error) {
         console.error(error);
     }
 };
 
-bot.command('/check', async (ctx) => {
-    console.log('Starting check');
-    plsStop = false;
-    check(ctx);
+bot.command('/stop', async () => {
+    console.log('stopping');
+    plsStop = true;
 });
 
-bot.command("/chat", async (ctx) => {
-    console.log(ctx.chat.id);
-})
-
-// bot.command('/percentagealert', async (ctx) => {
-//     console.log('Setting percentage floor');
-//     const args = ctx.update.message.text.split(' ');
-//     if (args.length != 2) {
-//         bot.telegram.sendMessage(
-//             ctx.chat.id,
-//             'Please input the correct format!\nE.g: /percentagealert 500',
-//             {}
-//         );
-//     } else {
-//         percentageFallFloor = parseFloat(args[1]);
-//         bot.telegram.sendMessage(
-//             ctx.chat.id,
-//             `Set the percentage alert floor to ${percentageFallFloor}`,
-//             {}
-//         );
-//     }
-// });
-
-// bot.command('/pricealert', async (ctx) => {
-//     console.log('Setting price floor');
-//     const args = ctx.update.message.text.split(' ');
-//     if (args.length != 2) {
-//         bot.telegram.sendMessage(
-//             ctx.chat.id,
-//             'Please input the correct format!\nE.g: /pricealert 2.1',
-//             {}
-//         );
-//     } else {
-//         priceFloor = parseFloat(args[1]);
-//         bot.telegram.sendMessage(
-//             ctx.chat.id,
-//             `Set the price alert floor to ${priceFloor}`,
-//             {}
-//         );
-//     }
-// });
-
-// bot.command('/stopcheck', async (ctx) => {
-//     console.log('Stopping check');
-//     plsStop = true;
-//     messageId = null;
-//     await bot.telegram.sendMessage(ctx.chat.id, 'I will stop now', {});
-// });
+check();
